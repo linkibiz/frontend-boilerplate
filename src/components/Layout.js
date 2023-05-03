a61a63ad10cd3e926550bacaacda4db9edb1bfad
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-05-03 00:32:58 -0500

    ajustes al head

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,15 @@
 import Head from 'next/head'
 
 const Layout = ({pageName, children}) => {
-  const title = `${pageName} - QUIJANO & ASSOCIATES`
+  const title = `${pageName} - Linki`
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <link rel='shortcut icon' href='/qa-logo.png'/>
+        <link rel='shortcut icon' href='/linki.png'/>
       </Head>
       <main className="h-full bg-slate-100 min-h-screen flex justify-center">
-        <div className="bg-white w-[768px] flex flex-col min-h-screen h-full overflow-hidden">
+        <div className="bg-white w-[425px] flex flex-col min-h-screen h-full overflow-hidden">
           {children}    
         </div>
       </main>
---
+++ src/pages/[slug]/perfil.js
@@ -0,0 +1,46 @@
+import Banner from "@/components/Banner";
+import ContactButtons from "@/components/ContactButtons";
+import Footer from "@/components/Footer";
+import Layout from "@/components/Layout";
+import Links from "@/components/Links";
+import Profile from "@/components/Profile";
+import SocialLinks from "@/components/SocialLinks";
+import Vcard from "@/components/Vcard";
+import Wrapper from "@/components/Wrapper";
+import { useRouter } from "next/router";
+import React from "react";
+
+const Perfil = ({ data }) => {
+  console.log(data)
+  const router = useRouter();
+  const { slug } = router.query;
+  const pageTitle = slug.split(".").join(" ").toUpperCase();
+  return (
+    <Layout pageName={pageTitle}>
+      <Banner banner={data}/>
+      <Wrapper>
+        <Profile profileData={data} />
+        <Vcard vcardData={data} />
+        <ContactButtons contactButtons={data} />
+        {/* <Links linksList={data.attributes.links} /> */}
+        <SocialLinks socialLinks={data.attributes.redes_sociales} /> 
+      </Wrapper>
+      {/* <Footer/> */}
+    </Layout>
+  );
+};
+
+export default Perfil;
+
+export async function getServerSideProps({ query: { slug } }) {
+  const url = `${process.env.NEXT_PUBLIC_STRAPI_URL}/profiles?filters[slug][$eq]=${slug}&populate=deep`;
+  console.log(url)
+  const req = await fetch(url);
+  const res = await req.json();
+  const data = res.data[0];
+  return {
+    props: {
+      data,
+    },
+  };
+}
