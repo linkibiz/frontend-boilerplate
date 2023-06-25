d4a941152bb1f6cb091351f4332d4fd22864c088
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-06-25 14:02:40 -0500

    ajustes

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -15,11 +15,11 @@ import React from "react";
 const Perfil = ({ data }) => {
   const router = useRouter();
   const { slug } = router.query;
-  console.log(data)
-  let pageTitle = slug.split("-").slice(0, -1).join(' ');
+  console.log(data);
+  let pageTitle = slug.split("-").slice(0, -1).join(" ");
   return (
     <Layout pageName={pageTitle}>
-      <Banner banner={data}/>
+      <Banner banner={data} />
       <Wrapper>
         <Profile profileData={data} />
         <div className="flex w-full">
@@ -31,7 +31,7 @@ const Perfil = ({ data }) => {
         {data.links.length > 0 && <Links linksList={data.links} />}
         {data.redes_sociales != null && <SocialLinks socialLinks={data.redes_sociales} />}
       </Wrapper>
-      
+
       {/* <Footer/> */}
     </Layout>
   );
@@ -40,15 +40,65 @@ const Perfil = ({ data }) => {
 export default Perfil;
 
 export async function getServerSideProps({ query: { slug } }) {
-  const url = `${process.env.NEXT_PUBLIC_STRAPI_URL}/users/${slug}?populate=deep`;
-  console.log(url)
+  const url = `${process.env.NEXT_PUBLIC_STRAPI_URL}/profiles?filters[slug][$eq]=${slug}&populate=deep`;
   const req = await fetch(url);
   const res = await req.json();
-  const data = res;
-  console.log(data)
+  const data = res.data[0];
   return {
     props: {
       data,
     },
   };
 }
+
+/*
+import About from "@/components/About";
+import Banner from "@/components/Banner";
+import ContactButtons from "@/components/ContactButtons";
+import Footer from "@/components/Footer";
+import Layout from "@/components/Layout";
+import Links from "@/components/Links";
+import Profile from "@/components/Profile";
+import SocialLinks from "@/components/SocialLinks";
+import Vcard from "@/components/Vcard";
+import Wrapper from "@/components/Wrapper";
+import { useRouter } from "next/router";
+import React from "react";
+
+const Perfil = ({ data }) => {
+  console.log(data.attributes.links.length);
+  const router = useRouter();
+  const { slug } = router.query;
+  const pageTitle = slug.split("-").join(" ").toUpperCase();
+  return (
+    <Layout pageName={pageTitle}>
+      <Banner banner={data.attributes.banner} />
+      <Wrapper>
+        <Profile profileData={data} />
+        {data.attributes.vcard != null && <Vcard vcardData={data} />}
+        {data.attributes.sobre_mi != "" && <About info={data} />}
+        {data.attributes.botones.length > 0 && <ContactButtons contactButtons={data} />}
+        {data.attributes.links.length > 0 && <Links linksList={data.attributes.links} />}
+        {data.attributes.redes_sociales != null && <SocialLinks socialLinks={data.attributes.redes_sociales} />}
+      </Wrapper>
+      
+      {/* <Footer/> 
+      </Layout>
+      );
+    };
+    
+    export default Perfil;
+    
+    export async function getServerSideProps({ query: { slug } }) {
+      const url = `${process.env.NEXT_PUBLIC_STRAPI_URL}/profiles?filters[slug][$eq]=${slug}&populate=deep`;
+      const req = await fetch(url);
+      const res = await req.json();
+      const data = res.data[0];
+      return {
+        props: {
+          data,
+        },
+      };
+    }
+    
+*/
