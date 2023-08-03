79e43078f9935d2a024b562a04e41c1c6f470c54
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-08-03 10:23:29 -0500

    ajustes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel='apple-touch-icon' sizes='180x180' href='/linki-black-logo.png'/>
+      </Head>
       <body>
         <Main />
         <NextScript />
