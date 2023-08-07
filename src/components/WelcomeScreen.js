c481d617cf465a8f203a4bae799526756aa0e0d0
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-08-07 10:04:44 -0500

    ajustes

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import React from "react";
-import Banner from "../../public/images/linki-welcome-banner.png";
 import WelcomeBanner from "./WelcomeBanner";
 const WelcomeScreen = ({onSubmit}) => {
   
