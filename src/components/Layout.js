1644eda385f909e007cca8cbf169efd07732c097
cristhian vargas <cristhian.a.vargas@gmail.com>
2024-01-15 15:04:51 -0500

    ajustes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,7 @@ const Layout = ({pageName, children}) => {
         <link rel='shortcut icon' href='/linki-black-logo.png'/>
       </Head>
       <main className="h-full bg-slate-100 flex justify-center">
-        <div className="bg-[#eceded] w-[425px] flex flex-col min-h-[100vh]">
+        <div className="bg-[#eceded] w-full max-w-[425px] flex flex-col min-h-[100vh]">
           {children}    
         </div>
       </main>
